17cc5fa35c95c4a573d3ea6576e73e5bef380b82
abdlrahman1 <92398027+abdlrahman1@users.noreply.github.com>
2021-10-22 00:51:51 +0200

    Add files via upload

---
+++ slideshow/index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+ <meta charset = "utf-8">
+     <meta name = "description" content = "welcome to my website">
+     <meta name = "viewport" content = "width=device-width,intial-scale=1.0">
+	 </head>
+	 
+	 <body>
+	 <?php
+	 include('slideshow.php');
+	  include('header.php');
+	
+	
+	?>
+	 
+	 </body>
+	 
+	 
+	 
+	 
+	 
+	 </html>
---
+++ slideshow/slideshow.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+   
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/slideshow.css"> </link>
+
+	
+</head>
+<body>
+
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="img/online-shopping-products.png" style="width:100%; height:280px;">
+  <div class="text">Caption Text</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="img/photo.JPEG" style="width:100%; height:280px;">
+  <div class="text">Caption Two</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="img/ecommerce-shopping-retail-ss-1920-800x450.jpg" style="width:100%; height:280px;">
+  <div class="text">Caption Three</div>
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>
+</html>
+
+<head>
+<script src="js/slideshow.js"></script>
+
+
+</head>
+  	
